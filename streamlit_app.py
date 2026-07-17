6ec0a90842cccfa3ec8071f017cddb1edbf7ac11
woodyackerson <woody.c.ackerson@gmail.com>
2026-07-16 22:05:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, '+ name_on_order + "!", icon="✅")
 
+# New secton to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
