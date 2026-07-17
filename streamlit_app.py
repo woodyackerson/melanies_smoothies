aaeb80be5c21b4b807b69400917a44165332057b
woodyackerson <woody.c.ackerson@gmail.com>
2026-07-16 22:22:02 -0500

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 
 # Display the response text or JSON in Streamlit
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
